a0ca8990833fab49741e1a11cafddd6eae966035
Micah Ferrill <mcferrill@gmail.com>
2017-01-27 12:13:54 -0600

    Cleaning up setup and layout.

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 
 from setuptools import setup, Extension
-import distutils.sysconfig
 import os
-import platform
 import sys
 
 
 # Find source and header files.
-sources = []
-headers = []
+sources, headers = [], []
 for pth, _, filenames in os.walk('libirig106/src'):
     for filename in filenames:
         filename = os.path.join(pth, filename)
         if filename.endswith(('.c')):
+
+            # Skip broken c files for now.
             if 'analog' in filename.lower() or 'stream' in filename:
-                print filename
                 continue
+
             sources.append(filename)
         elif filename.endswith('.h'):
             headers.append(filename)
@@ -26,54 +25,28 @@ if sys.platform == 'win32':
     link_flags = []
 else:
     flags = [
+        '-c', '-O3',
         '-fPIC', '-Wall', '-Wno-parentheses',  # '-Werror=switch',
         '-D_FILE_OFFSET_BITS=64',
         '-D_LARGEFILE64_SOURCE',
         '-ggdb',
         '-fpack-struct=1',
     ]
+    link_flags = ['-fPIC', '-lstdc++']
 
-    platform.mac_ver()
-    if platform.system() in ['Darwin', 'FreeBSD']:
-        os.environ.setdefault('CC', 'gcc')
-        os.environ.setdefault('CXX', 'gcc')
-        orig_customize_compiler = distutils.sysconfig.customize_compiler
-
-        def customize_compiler(compiler):
-            orig_customize_compiler(compiler)
-            compiler.compiler[0] = os.environ['CC']
-            compiler.compiler_so[0] = os.environ['CXX']
-            compiler.compiler_cxx[0] = os.environ['CXX']
-            compiler.linker_so[0] = os.environ['CXX']
-            return compiler
-        distutils.sysconfig.customize_compiler = customize_compiler
-        flags.append('-stdlib=libc++')
-        if platform.system() == 'Darwin':
-            flags.append('-mmacosx-version-min=10.7',)
-            # if tuple(map(int, platform.mac_ver()[0].split('.'))) >= (10, 9):
-            #     flags.append(
-            #         '-Wno-error=unused-command-line-argument-hard-error-in-future',  # noqa
-            #     )
-
-    flags = ['-c', '-O3'] + flags
-
-    if platform.system() == 'FreeBSD':
-        link_flags = ['-fPIC', '-lc++']
-    else:
-        link_flags = ['-fPIC', '-lstdc++']
-
-
-c_extension = Extension(
+_irig106 = Extension(
     '_irig106',
     sources,
-    # include_dirs=[os.path.join('.', 'src')],
     depends=headers,
     extra_compile_args=flags,
     extra_link_args=link_flags,
+
+    # This causes errors on unixy systems.
+    # include_dirs=[os.path.join('libirig106', 'src')],
 )
 
 setup(
     name='libirig106',
-    ext_modules=[c_extension],
-    packages=['Py106'],
+    ext_modules=[_irig106],
+    packages=['irig106'],
 )
