05cc8faac5202d7dabde338f56e4e8d97b8d3d94
Micah Ferrill <mcferrill@gmail.com>
2017-01-27 12:47:22 -0600

    Renamed for pep8 compliance.

--- irig106/__init__.py
+++ irig106/__init__.py
@@ -4,23 +4,23 @@ Py106 - This python package provides an interface to the IRIG 106 data DLL.
 
 Message data structures are based on the ctypes module.  The main implication of
 this is that to use message data, first the data buffer needs to be cast to the appropriate
-data structure. 
+data structure.
 
 Example 1
 
     # Import the irig106 package
     import Py106
-    
+
     # Create IRIG IO object
     IrigIO  = Py106.Packet.IO()
-    
+
     # Open data file for reading
     IrigIO.open("data.ch10", IrigIO.FileMode.READ)
-    
+
     # Read IRIG headers
     for PktHdr in IrigIO.packet_headers():
         print "Ch ID %3i  %s" % (IrigIO.Header.ChID, Py106.Packet.DataType.TypeName(PktHdr.DataType))
-                
+
     IrigIO.close()
 
 
@@ -47,7 +47,7 @@ try:
     import Packet
     import Status
     import Time
-    
+
 except ImportError:
     print "Py106 init error - Error importing modules"
 
