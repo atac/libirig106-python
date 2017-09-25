76f34551b15515e63d7ebbecd80bad3c026bd8cd
Micah Ferrill <mcferrill@gmail.com>
2017-09-25 14:21:01 -0500

    Initial compile-import-run established.

--- _irig106.c
+++ _irig106.c
@@ -1,11 +1,26 @@
 
 #include <Python.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-PyMODINIT_FUNC
-init_irig106(void){
-    /* PyObject *m; */
 
-    /* m = Py_InitModule("spam", SpamMethods); */
-    /* if (m == NULL) */
-    /*     return; */
+static PyObject *hello(PyObject *self){
+    printf("Hello from c!\n");
+
+    Py_RETURN_NONE;
+}
+
+static char i106_docstring[] = "This is a placeholder.";
+static PyMethodDef i106_methods[] = {
+    {"hello", (PyCFunction)hello, METH_NOARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+void i106_init_module(PyObject *module){}
+
+PyMODINIT_FUNC init_i106(void){
+    PyObject *m = Py_InitModule3("_i106", i106_methods, i106_docstring);
+    if (m == NULL)
+        return;
 }
--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 
 from setuptools import setup, Extension
-import os
+from glob import glob
+# import os
 import sys
 
 
 # Find source and header files.
 sources, headers = ['_irig106.c'], []
-for pth, _, filenames in os.walk('libirig106/src'):
-    for filename in filenames:
-        filename = os.path.join(pth, filename)
-        if filename.endswith(('.c')):
-
-            # Skip broken c files for now.
-            if 'analog' in filename.lower() or 'stream' in filename:
-                continue
-
-            sources.append(filename)
-        elif filename.endswith('.h'):
-            headers.append(filename)
+sources += glob('libirig106/src/*.c')
+headers += glob('libirig106/src/*.h')
 
 # Define flags based on platform.
 if sys.platform == 'win32':
@@ -25,28 +16,26 @@ if sys.platform == 'win32':
     link_flags = []
 else:
     flags = [
-        '-c', '-O3',
-        '-fPIC', '-Wall', '-Wno-parentheses',  # '-Werror=switch',
+        '-c', '-std=c99',
+        '-fPIC',  '-Wall',
         '-D_FILE_OFFSET_BITS=64',
         '-D_LARGEFILE64_SOURCE',
         '-ggdb',
         '-fpack-struct=1',
     ]
-    link_flags = ['-fPIC', '-lstdc++']
+    link_flags = ['-fPIC']
 
-_irig106 = Extension(
-    '_irig106',
+_i106 = Extension(
+    '_i106',
     sources,
     depends=headers,
     extra_compile_args=flags,
     extra_link_args=link_flags,
-
-    # This causes errors on unixy systems.
-    # include_dirs=[os.path.join('libirig106', 'src')],
 )
 
 setup(
     name='libirig106',
-    ext_modules=[_irig106],
+    version='0.0.1',
+    ext_modules=[_i106],
     packages=['irig106'],
 )
