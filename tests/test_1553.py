307ef1c9fdf82ebde29d67549ad41c579ce573cb
Micah Ferrill <mcferrill@gmail.com>
2018-01-05 15:29:57 -0600

    Added 1553 test module.

---
+++ tests/test_1553.py
@@ -0,0 +1,16 @@
+
+from i106 import C10
+import pytest
+
+
+class Test1553:
+
+    @pytest.fixture
+    def p(self):
+        for packet in C10('1553.c10'):
+            if packet.data_type == 25:
+                return packet
+
+    def test_iteration(self, p):
+        for i, msg in enumerate(p):
+            print (i)
