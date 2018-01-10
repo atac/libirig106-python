ccb6bff7b7c12d2e631a306233ff78ee507d9342
Micah Ferrill <mcferrill@gmail.com>
2018-01-09 18:15:46 -0600

    Added 1553 test data file to repo.

--- tests/test_1553.py
+++ tests/test_1553.py
@@ -7,11 +7,10 @@ class Test1553:
 
     @pytest.fixture
     def p(self):
-        for packet in C10('1553.c10'):
+        for packet in C10('tests/1553.c10'):
             if packet.data_type == 25:
                 return packet
 
     def test_iteration(self, p):
-        # print ('%s messages' % len(p))
         for i, msg in enumerate(p):
             print (i)
